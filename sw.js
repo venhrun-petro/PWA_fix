37172d4917b96fb517aa4a04c10448a8f52748d2
Petro_Venhrun <p.venhrun@pozitiff.ua>
2018-02-02 16:39:30 +0200

    'sadsd'

--- sw.js
+++ sw.js
@@ -58,14 +58,4 @@ function refresh(response) {
 });
 });
 }
-'use strict';
 
-importScripts('sw-toolbox.js');
-
-toolbox.precache(["index.html","compiled.min.css"]);
-
-toolbox.router.get('/images/*', toolbox.cacheFirst);
-
-toolbox.router.get('/*', toolbox.networkFirst, {
-    networkTimeoutSeconds: 5
-});
