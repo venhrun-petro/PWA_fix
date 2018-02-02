178e5e10dd587f0757acc8be8b4669755dacda0e
Petro_Venhrun <p.venhrun@pozitiff.ua>
2018-02-02 14:04:40 +0200

    'sadsd'

--- sw.js
+++ sw.js
@@ -5,25 +5,26 @@
 
 
 
-
-
-
-const CACHE = 'cache-and-update-v1';
+const CACHE = 'cache-update-and-refresh-v1';
 
 // При установке воркера мы должны закешировать часть данных (статику).
 self.addEventListener('install', (event) => {
     event.waitUntil(
-    caches.open(CACHE).then((cache) =>
-    .then((cache) => cache.addAll(["index.html","compiled.min.css"]))
+    caches
+        .open(CACHE)
+        .then((cache) => cache.addAll(["index.html","compiled.min.css"]))
 );
 });
 
-// при событии fetch, мы используем кэш, и только потом обновляем его данным с сервера
-self.addEventListener('fetch', function(event) {
-    // Мы используем `respondWith()`, чтобы мгновенно ответить без ожидания ответа с сервера.
+// При запросе на сервер мы используем данные из кэша и только после идем на сервер.
+self.addEventListener('fetch', (event) => {
+    // Как и в предыдущем примере, сначала `respondWith()` потом `waitUntil()`
     event.respondWith(fromCache(event.request));
-    // `waitUntil()` нужен, чтобы предотвратить прекращение работы worker'a до того как кэш обновиться.
-    event.waitUntil(update(event.request));
+event.waitUntil(
+    update(event.request)
+    // В конце, после получения "свежих" данных от сервера уведомляем всех клиентов.
+        .then(refresh)
+);
 });
 
 function fromCache(request) {
@@ -36,7 +37,24 @@ function fromCache(request) {
 function update(request) {
     return caches.open(CACHE).then((cache) =>
         fetch(request).then((response) =>
-        cache.put(request, response)
+        cache.put(request, response.clone()).then(() => response)
 )
 );
+}
+
+// Шлём сообщения об обновлении данных всем клиентам.
+function refresh(response) {
+    return self.clients.matchAll().then((clients) => {
+        clients.forEach((client) => {
+        // Подробнее про ETag можно прочитать тут
+        // https://en.wikipedia.org/wiki/HTTP_ETag
+        const message = {
+            type: 'refresh',
+            url: response.url,
+            eTag: response.headers.get('ETag')
+        };
+    // Уведомляем клиент об обновлении данных.
+    client.postMessage(JSON.stringify(message));
+});
+});
 }
